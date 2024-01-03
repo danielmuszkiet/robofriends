69ccd560b1ca7270824f5501d20b064a647df1d3
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2024-01-03 11:50:59 +0100

    card bg color changed

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ id, name, email }) => {
   return (
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow shadow-5">
+    <div className="tc bg-white-40 dib br3 pa3 ma2 grow shadow-5">
       <img src={`https://robohash.org/daniel${id}?size=220x220`} alt="robots" />
       <div>
         <h3>{name}</h3>
