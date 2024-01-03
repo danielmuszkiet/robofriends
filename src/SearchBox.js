bcc1dc6125ef3fffa6416520d88fa7b220b3c9cb
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2024-01-03 11:46:08 +0100

    searchbox style change

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -4,9 +4,9 @@ const SearchBox = ({ searchChange }) => {
   return (
     <div className="pa2">
       <input
-        className="pa3 ma2 w-25 ba b--gray bg-near-white"
+        className="pa3 ma2 w-25 br3 tc ba b--gray bg-near-white"
         type="search"
-        placeholder="search robots"
+        placeholder="Search robots here"
         onChange={searchChange}
       />
     </div>
